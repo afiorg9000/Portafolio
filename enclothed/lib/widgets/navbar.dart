ea0e6f2070e433fa6bdf2cdcd670e3855ba427ca
Drew-24 <andrewwil1@hotmail.com>
2022-06-08 12:48:02 -0400

    Added enclothed name and vocal on widget

--- enclothed/lib/widgets/navbar.dart
+++ enclothed/lib/widgets/navbar.dart
@@ -8,17 +8,34 @@ class NavBar extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 38),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              gradient: const LinearGradient(colors: [
-                Color(0xFFC86DD7),
-                Color(0xFF3023AE),
-              ], begin: Alignment.bottomRight, end: Alignment.topLeft),
-            ),
+        children: [
+          Row(
+            children: <Widget>[
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  gradient: const LinearGradient(colors: [
+                    Color(0xFFC86DD7),
+                    Color(0xFF3023AE),
+                  ], begin: Alignment.bottomRight, end: Alignment.topLeft),
+                ),
+                child: const Center(
+                  child: Text(
+                    "E",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              const Text(
+                "Enclothed",
+                style: TextStyle(fontSize: 26),
+              ),
+            ],
           ),
         ],
       ),
